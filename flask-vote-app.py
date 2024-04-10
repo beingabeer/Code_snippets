5c291351b9eec3d5de590d1366a93a7faa0d97b6
Abeer <42182770+beingabeer@users.noreply.github.com>
2024-04-09 21:22:25 -0400

    Update flask-vote-app.py

--- flask-vote-app.py
+++ flask-vote-app.py
@@ -1,37 +1,40 @@
-# app.py
 from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit
 import redis
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 languages = {
-    'Python': 0,
-    'JavaScript': 0,
-    'Java': 0,
-    'C++': 0,
-    'Go': 0
+    'Python': 'python_votes',
+    'JavaScript': 'javascript_votes',
+    'Java': 'java_votes',
+    'C++': 'cpp_votes',
+    'Go': 'go_votes'
 }
 
 @app.route('/')
 def index():
-    return render_template('index.html', languages=languages)
+    language_votes = {}
+    for language, key in languages.items():
+        vote_count_bytes = redis_client.get(key)
+        vote_count = int(vote_count_bytes.decode()) if vote_count_bytes is not None else 0
+        language_votes[language] = vote_count
+    return render_template('index.html', language_votes=language_votes)
 
 @app.route('/vote', methods=['POST'])
 def vote():
     language = request.json['language']
     if language in languages:
-        redis_client.incr(language)
-        languages[language] += 1
+        redis_key = languages[language]
+        redis_client.incr(redis_key)
+        new_count = int(redis_client.get(redis_key).decode())
+        emit('vote_update', {'language': language, 'count': new_count}, broadcast=True)
         return jsonify({'success': True})
     else:
         return jsonify({'success': False, 'error': 'Invalid language'})
 
-@app.route('/results')
-def results():
-    language_votes = {lang.decode(): int(redis_client.get(lang)) for lang in languages.keys()}
-    sorted_votes = sorted(language_votes.items(), key=lambda x: x[1], reverse=True)
-    return render_template('results.html', sorted_votes=sorted_votes)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
